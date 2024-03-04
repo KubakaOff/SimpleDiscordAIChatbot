9e37be6200d72f15fb220ec1302f0641f9d940ce
KubakaOff <108632438+KubakaOff@users.noreply.github.com>
2024-03-04 18:58:41 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 # 2. Verify (if you have to) and go to the channel "#bot"
 # 3. Type in /key
 # 4. Copy the key here
-openai_api_key = "KEY FROM discord.pawan.krd"
+openai_api_key = "pk-vzUMtsqOEBwgmVIWZrsskaAVqmVCNIvTExhSdNPjlzftDmjD"
 # 5. Install py-cord, tiktoken and openai (python -m pip install py-cord tiktoken openai)
 # 6. Go to the Discord Developer Portal and Generate a bot token
 # 7. Enter the token below
-discord_token = "TOKEN FROM DISCORD DEV PORTAL"
+discord_token = "OTIxNzMwNDI2MDQ3OTU0OTY1.GRGPWT.qlhG8DgIUA37p1AWtYj4AYq03IajRO3lHWSrD8"
 # 8. Uncomment one of the models with the usage below the instruction
 # 9. Configure rest of the stuff, the comments are there to explain everything
 # 10. Done!
@@ -22,12 +22,21 @@ usage = 0.25
 # model = "pai-001"
 # usage = 0.5
 #
+# PAWAN SUPPORTER ONLY - GPT-3.5-TURBO - Uses 0.25 credits per 1000 tokens - Max tokens = 4096
+# (if you REALLY want gpt-4 and gpt-3.5 filtered support, you can make a PR with error handling, it's not my priority right now)
+# model = "gpt-3.5-unfiltered"
+# usage = 0.25
+# Please do not report if it isn't working, it's a pawan or you issue. (make sure you actually have the Supporter role and check the Pawan server for downtime.)
+#
 #
 # Should the model be in roleplay mode? (only applicable to pai models)
 # In roleplay mode the bot is more likely to generate NSFW (but the no roleplay version can generate NSFW too)
 # Roleplay mode is also behaving more like a human, has it's own "feelings", "opinions" etc. (even though they're fake)
 # Note: Roleplay mode only works for pai-001 and pai-001-light
 roleplay = False
+#
+# Do you want to show the currently used model when running the "status" command?
+show_currently_used_model = True
 #
 # Can server admins/mods change the model variants between generic and roleplay?
 # This won't affect your billing.
@@ -48,13 +57,13 @@ nocontext_prompt = prompt
 prompt_change = True
 #
 # This is the max context, you get 250 credits daily, pai-001-light models use 0.25 credits per 1000, and pai-001 use 0.5 per 1000
-max_context = 2000
+max_context = 1024
 #
 # Amount of messages to get, if I were you, I would just set it to max_context / 200 or max_context / 150, maybe even max_context / 100
 num_messages = int(round(max_context / 100))
 #
 # Length of response, one token is around 3-4 characters, so the max is around 500-600, I would recommend something like 200, depends on use case
-response_length = 200
+response_length = 500
 #
 # If the bot should repeat more, but be more intelligent, or repeat less, but be less inteligent, the starting point is 0.7, play with it until
 # you get the desired inteligence to repetition ratio
