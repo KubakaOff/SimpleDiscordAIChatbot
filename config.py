51e16e3e679a6e5f0e7ff2d517c011258b48fdf9
KubakaOff <108632438+KubakaOff@users.noreply.github.com>
2024-04-20 11:23:29 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # 2. Verify (if you have to) and go to the channel "#bot"
 # 3. Type in /key
 # 4. Copy the key here
-api_key = "KEY FROM groqcloud.com"
+api_key = "KEY FROM console.groq.com"
 # 5. Install py-cord, tiktoken and openai (python -m pip install py-cord tiktoken openai)
 # 6. Go to the Discord Developer Portal and Generate a bot token
 # 7. Enter the token below
@@ -13,11 +13,10 @@ discord_token = "TOKEN FROM DISCORD DEV PORTAL"
 # 10. Done!
 # IMPORTANT! IF YOU GET ANY ERRORS WHEN GENERATING A RESPONSE, RUN /resetip IN THE #bot CHANNEL IN THE PAWAN SERVER YOU JOINED EARLIER
 #
-# Do you want to show the currently used model when running the "status" command?
-show_currently_used_model = True
 # Prefix for commands in the bot
 command_prefix = "ai!"
 #
+model = ""
 #
 # Prompt for the AI to follow (as system message). Set to None for no prompt
 prompt = None
@@ -28,21 +27,18 @@ nocontext_prompt = prompt
 # Can server admins/mods change the prompts for their server?
 prompt_change = True
 #
-# This is the max context, you get 250 credits daily, pai-001-light models use 0.25 credits per 1000, and pai-001 use 0.5 per 1000
+# Use as much as you can afford.
 max_context = 8000
 #
 # Amount of messages to get, if I were you, I would just set it to max_context / 200 or max_context / 150, maybe even max_context / 100
-num_messages = int(round(max_context / 100))
+num_messages = int(round(max_context / 250))
 #
 # Length of response, one token is around 3-4 characters, so the max is around 500-600, I would recommend something like 200, depends on use case
-response_length = 2000
+response_length = 600
 #
 # If the bot should repeat more, but be more intelligent, or repeat less, but be less inteligent, the starting point is 0.7, play with it until
-# you get the desired inteligence to repetition ratio
+# you get the desired intelligence to repetition ratio
 temperature = 0.7
-#
-# If you want to use a different model for chatting without context, enter it here as a string
-nocontext_model = None
 #
 # If you want a different response length for chatting without context, enter it here as an integer
 nocontext_response_length = None
@@ -51,10 +47,7 @@ nocontext_response_length = None
 # Keep in mind that it will make the bot less "intelligent".
 nocontext_temperature = None
 #
-# If you don't want the "status" command, change it to False.
-status_command = True
-#
-# If you don't want to use Pawans API, change this to the base_url you want, and set "status_command" to False
+# If you don't want to use Groq cloud, change this to the OpenAI compatible base_url you want.
 openai_base_url = "https://api.groq.com/openai/v1"
 #
 # Filenames/paths for saving settings:
@@ -66,13 +59,11 @@ prompts_filename = "prompts.json"
 help_command = f"""
 **AI Bot Help**
 ```{command_prefix}help - You are here
-{command_prefix}chat [message] - Send a message to the bot
-{command_prefix}status - Show remaining credits for today for the whole bot```
+{command_prefix}chat [message] - Send a message to the bot```
 **Admin Commands**
 ```{command_prefix}chatbotchannel [channel] - Add/remove a channel where the AI can chat.
-{command_prefix}switchmodel (channel) - Switch to/from roleplay to generic models.
 {command_prefix}setprompt [prompt] (channel) - Set prompt to prompt for the whole server or only for one channel.```
 
 You can host your own AI Discord Bot from here: https://github.com/KubakaOff/SimpleDiscordAIChatbot
-You can also [add the main instance to your own Discord server](https://discord.com/oauth2/authorize?client_id=921730426047954965&permissions=274881154048&scope=bot)!
+You can also [add the main instance to your own Discord server](https://discord.com/oauth2/authorize?client_id=921730426047954965&permissions=274881154048&scope=bot) (running on Pawan)
 """
